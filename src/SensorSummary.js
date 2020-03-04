d03cd472b229dde316528d99f932d371fbe3daf2
Charlotte <charlotte.groder@gmail.com>
2020-03-04 13:56:15 +0100

    Printer kun ut siste verdi fra db

--- src/SensorSummary.js
+++ src/SensorSummary.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 const SensorSummary = ({ sensor }) => {
+	let gjeldendeVerdi = 0;
+	let valueArr = [];
+	for (let i = 0; i < sensor.value.length; i++) {
+		gjeldendeVerdi = sensor.value[i];
+		//console.log('gverdi', gjeldendeVerdi);
+		valueArr.push(gjeldendeVerdi); //hvis man skal se resultater over tid kan man bruke valueArr
+	}
 	return (
 		<div className="col l3 card project-summary blue-grey darken-1">
 			<div className="card-content white-text" id="cardcontent">
 				<p className="card-title">{sensor.name}</p>
-				<p> Value: {sensor.value}</p>
+
+				<p> Value: {gjeldendeVerdi}</p>
 			</div>
 		</div>
 	);
